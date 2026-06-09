9733edc8fad2c8188a9f4ccbc406f9ddfeeb8edd
Theobobble <theomatzen@gmail.com>
2026-06-08 20:41:07 -0700

    Update CartographyTableMenuMixin - simplify

--- src/main/java/org/cardboardpowered/mixin/world/inventory/CartographyTableMenuMixin.java
+++ src/main/java/org/cardboardpowered/mixin/world/inventory/CartographyTableMenuMixin.java
@@ -1,28 +1,11 @@
 package org.cardboardpowered.mixin.world.inventory;
 
-import org.bukkit.craftbukkit.inventory.CraftInventoryView;
 import net.minecraft.world.inventory.CartographyTableMenu;
-import org.bukkit.entity.Player;
-import org.bukkit.craftbukkit.inventory.CraftInventoryCartography;
 import org.spongepowered.asm.mixin.Mixin;
 
 import org.cardboardpowered.bridge.world.inventory.CartographyTableMenuBridge;
-import org.cardboardpowered.bridge.world.inventory.ContainerLevelAccessBridge;
-import org.cardboardpowered.bridge.server.level.ServerPlayerBridge;
 
 @Mixin(CartographyTableMenu.class)
 public class CartographyTableMenuMixin extends ItemCombinerMenuMixin implements CartographyTableMenuBridge {
-
-    public CraftInventoryView bukkitEntity;
-
-    @Override
-    public CraftInventoryView getBukkitView() {
-        if (bukkitEntity != null)
-            return bukkitEntity;
-
-        org.bukkit.craftbukkit.inventory.CraftInventory inventory = new CraftInventoryCartography(
-                ((ContainerLevelAccessBridge)access).getLocation(), this.inputSlots, this.resultSlots);
-        bukkitEntity = new CraftInventoryView((Player)((ServerPlayerBridge)this.player).getBukkitEntity(), inventory, (CartographyTableMenu)(Object)this);
-        return bukkitEntity;
-    }
+    // Cartography table menu implementation
 }
